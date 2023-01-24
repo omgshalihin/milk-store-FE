e6e0a55268b73a2f8fc161501861a32443f18533
omgshalihin <shalihinsgp@gmail.com>
2023-01-24 13:06:42 +0100

    fetching data using loader from react router

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -14,63 +14,54 @@ import {
 } from "@mantine/core";
 import { IconShoppingCartPlus } from "@tabler/icons";
 import React, { FC, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 import { iProduct } from "../iProduct";
 import styles from "./Product.module.css";
 
 const Product: FC = () => {
-  const [value, setValue] = useState(100);
-  const params = useParams();
-  // const [productId, setProductId] = useState<string>();
+  const product: any = useLoaderData();
 
-  useEffect(() => {
-    const productId = params.id;
-  }, []);
+  const [value, setValue] = useState(100);
 
   return (
     <>
-      <h1>Product Details!</h1>
-      <p>{params.id}</p>
-      <p></p>
-      <>
-        <Container py="xl">
-          <SimpleGrid cols={2} breakpoints={[{ maxWidth: "sm", cols: 1 }]}>
-            <Card p="md" radius="md" component="a">
-              <AspectRatio ratio={1 / 1}>
-                <Image src="/milk.png" />
-              </AspectRatio>
-            </Card>
-            <Flex direction="column" gap="xl" justify="space-between">
-              <div>
-                <Text align="left" size="xl">
-                  Name
-                </Text>
-                <Text color="dimmed" align="left" size="xl">
-                  Type
-                </Text>
-                <Text color="dimmed" align="left" size="xl">
-                  Storage Qty
-                </Text>
-              </div>
+      <Container py="xl">
+        <SimpleGrid cols={2} breakpoints={[{ maxWidth: "sm", cols: 1 }]}>
+          <Card p="md" radius="md" component="a">
+            <AspectRatio ratio={1 / 1}>
+              <Image src="/milk.png" />
+            </AspectRatio>
+          </Card>
+          <Flex direction="column" gap="xl" justify="space-between">
+            <div>
+              <Text align="left" size="xl">
+                {product.name}
+              </Text>
+              <Text color="dimmed" align="left" size="xl">
+                {product.type}
+              </Text>
+              <Text color="dimmed" align="left" size="xl">
+                {product.storage}
+              </Text>
+            </div>
 
-              <Slider
-                value={value}
-                onChange={setValue}
-                size="xl"
-                marks={[
-                  { value: 20, label: "20%" },
-                  { value: 50, label: "50%" },
-                  { value: 80, label: "80%" },
-                ]}
-              />
+            <Slider
+              value={value}
+              onChange={setValue}
+              size="xl"
+              marks={[
+                { value: 20, label: "20%" },
+                { value: 50, label: "50%" },
+                { value: 80, label: "80%" },
+              ]}
+            />
 
-              <Button size="xl" leftIcon={<IconShoppingCartPlus size={30} />}>
-                <Text>Order {value} liter</Text>
-              </Button>
-            </Flex>
-          </SimpleGrid>
-        </Container>
-      </>
+            <Button size="xl" leftIcon={<IconShoppingCartPlus size={30} />}>
+              <Text>Order {value} liter</Text>
+            </Button>
+          </Flex>
+        </SimpleGrid>
+      </Container>
     </>
   );
 };
